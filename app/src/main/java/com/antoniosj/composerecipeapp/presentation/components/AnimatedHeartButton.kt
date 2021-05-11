29b84fa03479161db9793fa1ea3ce1bb245ed54f
Antonio Silva <antonio.silvajr23@gmail.com>
2021-05-11 15:33:03 -0300

    Update heart button component

--- app/src/main/java/com/antoniosj/composerecipeapp/presentation/components/AnimatedHeartButton.kt
+++ app/src/main/java/com/antoniosj/composerecipeapp/presentation/components/AnimatedHeartButton.kt
@@ -1,78 +1,91 @@
 package com.antoniosj.composerecipeapp.presentation.components
 
-
-import android.util.Log
-import androidx.compose.animation.core.*
+import androidx.compose.animation.core.animateDp
+import androidx.compose.animation.core.updateTransition
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
+
+
 import androidx.compose.foundation.layout.size
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Favorite
-import androidx.compose.material.icons.filled.FavoriteBorder
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.composed
+import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
-
-@Composable
-fun AnimatedHeartButton(
-    modifier: Modifier,
-    buttonState: MutableState<HeartAnimationDefinition.HeartButtonState>,
-    onToggle: () -> Unit)
-{
-
-    val size by animateDpAsState(
-        // Animation won't work if target value is always equal, this may be a jetpack compose bug?
-        if(buttonState.value == HeartAnimationDefinition.HeartButtonState.ACTIVE) 50.1.dp else 50.dp,
-        animationSpec = keyframes {
-            durationMillis = 500
-            HeartAnimationDefinition.expandedIconSize.at(100)
-            HeartAnimationDefinition.idleIconSize.at(200)
-        },
-        finishedListener = {
-            Log.d("DEBUG", "TEST ANIMATION")
-        }
-    )
-
-    HeartButton(
-        modifier = modifier,
-        buttonState = buttonState,
-        onToggle = onToggle,
-        size = size
-    )
-}
-
-@Composable
-private fun HeartButton(
-    modifier: Modifier,
-    buttonState: MutableState<HeartAnimationDefinition.HeartButtonState>,
-    onToggle: () -> Unit,
-    size: Dp
-) {
-    if(buttonState.value == HeartAnimationDefinition.HeartButtonState.ACTIVE) {
-        Image(
-            imageVector = Icons.Default.Favorite,
-            modifier = modifier
-                .size(size)
-                .noRippleClickable(onClick = onToggle),
-            contentDescription = "")
-    }
-    else {
-        Image(
-            imageVector = Icons.Default.FavoriteBorder,
-            modifier = modifier
-                .size(size)
-                .noRippleClickable(onClick = onToggle),
-            contentDescription = "")
-    }
-}
+import com.antoniosj.composerecipeapp.presentation.components.HeartAnimationDefinition.expandedIconSize
+import com.antoniosj.composerecipeapp.presentation.components.HeartAnimationDefinition.idleIconSize
+import com.antoniosj.composerecipeapp.utils.loadDrawable
+import com.antoniosj.composerecipeapp.R
+import com.antoniosj.composerecipeapp.presentation.components.HeartAnimationDefinition.expandedIconImage
+import com.antoniosj.composerecipeapp.presentation.components.HeartAnimationDefinition.idleIconImage
 
 object HeartAnimationDefinition {
 
-    enum class HeartButtonState { IDLE, ACTIVE }
-
+    enum class HeartButtonState {
+        IDLE, ACTIVE
+    }
     val idleIconSize = 50.dp
     val expandedIconSize = 80.dp
+    const val idleIconImage = R.drawable.heart_grey
+    const val expandedIconImage = R.drawable.heart_red
+}
+
+@Composable
+fun HeartButton(
+    modifier: Modifier,
+    buttonState: MutableState<HeartAnimationDefinition.HeartButtonState>,
+    onToggle: () -> Unit
+) {
+    val transitionData = AnimatedHeartBtn(buttonState)
+
+    if (buttonState.value == HeartAnimationDefinition.HeartButtonState.ACTIVE) {
+        loadDrawable(drawable = expandedIconImage).value?.let {
+            Image(
+                bitmap = it.asImageBitmap(),
+                modifier = modifier
+                    .noRippleClickable(onClick = onToggle)
+                    .size(transitionData.size),
+                contentDescription = "")
+        }
+    } else {
+        loadDrawable(drawable = idleIconImage).value?.let {
+            Image(
+                bitmap = it.asImageBitmap(),
+                modifier = modifier
+                    .noRippleClickable(onClick = onToggle)
+                    .size(transitionData.size),
+                contentDescription = "")
+        }
+    }
+
+
+}
+
+private class TransitionData(
+    size: State<Dp>
+) {
+    val size by size
+}
+
+@Composable
+private fun AnimatedHeartBtn(
+    buttonState: MutableState<HeartAnimationDefinition.HeartButtonState>,
+): TransitionData  {
+    val transition = updateTransition(buttonState, label = "")
+    val size = transition.animateDp { state ->
+        when (state.value) {
+            HeartAnimationDefinition.HeartButtonState.IDLE -> idleIconSize
+            HeartAnimationDefinition.HeartButtonState.ACTIVE -> expandedIconSize
+        }
+    }
+    return remember(transition) { TransitionData(size) }
+}
+
+inline fun Modifier.noRippleClickable(crossinline onClick: ()->Unit): Modifier = composed {
+    clickable(indication = null,
+        interactionSource = remember { MutableInteractionSource() }) {
+        onClick()
+    }
 }
--- app/src/main/java/com/antoniosj/composerecipeapp/presentation/ui/recipelist/RecipeListFragment.kt
+++ app/src/main/java/com/antoniosj/composerecipeapp/presentation/ui/recipelist/RecipeListFragment.kt
@@ -4,37 +4,18 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.itemsIndexed
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.text.KeyboardActions
-import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.*
-import androidx.compose.material.TextFieldDefaults.textFieldColors
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Search
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.ComposeView
-import androidx.compose.ui.platform.LocalSoftwareKeyboardController
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.input.ImeAction
-import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.navigation.findNavController
-import com.antoniosj.composerecipeapp.R
 import com.antoniosj.composerecipeapp.presentation.components.*
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.launch
 import com.antoniosj.composerecipeapp.presentation.components.HeartAnimationDefinition.HeartButtonState.*
 
 @AndroidEntryPoint
@@ -75,12 +56,25 @@ class RecipeListFragment: Fragment() {
                         onSelectedCategoryChanged = viewModel::onSelectedCategoryChanged,
                         onChangeCategoryScrollPosition = viewModel::onChangeCategoryScrollPosition
                     )
-                    val state = remember { mutableStateOf(HeartAnimationDef.HeartBtnState.IDLEE) }
-                    HeartButton(modifier = Modifier,
-                        buttonState = state,
-                        onToggle = {
-                            state.value = if(state.value == HeartAnimationDef.HeartBtnState.IDLEE) HeartAnimationDef.HeartBtnState.ACTIVEE else HeartAnimationDef.HeartBtnState.IDLEE
-                        },)
+
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .height(200.dp),
+                        horizontalArrangement = Arrangement.Center
+                    ) {
+
+                        val state = remember { mutableStateOf(IDLE) }
+
+                        HeartButton(modifier = Modifier,
+                            buttonState = state,
+                            onToggle = {
+                                state.value = if(state.value == IDLE) ACTIVE else IDLE
+                            }
+                        )
+                    }
+                    
+
                     //PulsingDemo() // Testing my component
 //                    Box(
 //                        modifier = Modifier.fillMaxSize()
